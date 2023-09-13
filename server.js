c32d2f156680c6e017fc9b4d3397c4b703536f1d
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-09-13 18:50:41 -0400

    models and views updated

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require('express');
 require('dotenv').config();
 const colors = require('colors');
+const drinks = require('./models/drinks');
 const morgan = require('morgan');
 const PORT = process.env.PORT;
 //Initialization.....
 const app = express();
 
-
+//Middleware....
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
 
 //Routes....
+//Index Route...
+app.get('/drinks', (req, res) => {
+    res.render('index.ejs', {
+        allDrinks : drinks
+    })
+})
 
+//Show Route...
+app.get('/drinks/:id', (req, res) => {
+    res.render('show.ejs', {
+        drink : drinks[req.params.id]
+    })
+})
 
 //Listening.....
 app.listen(PORT, () => {
